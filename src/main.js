70ffb19c3354376920fb793540bf866d40094d1e
Guillaume <guillaume.vincent1138@gmail.com>
2024-11-07 18:06:30 +0100

    format document

--- src/main.js
+++ src/main.js
@@ -1,35 +1,33 @@
 import "./styles/style.css";
 import Nav from "./components/Nav.js";
-import createCategory from "./components/Category.js";
-import createFilterBtn from "./components/FilterBar.js";
 import displayProducts from "./components/Carte.js";
-import { displayProduct } from "./components/Detailspage.js";
-import { createBrowserHistory } from "history";
+import createCategory from './components/Category';
+import { displayProduct } from './components/Detailspage.js'
+import createFilterBtn from "./components/FilterBar.js";
+// import { createBrowserHistory } from "history";
 // import filter from './utils/Filter.js';
 
 document.getElementById("nav").appendChild(Nav());
-document.getElementById("nav").appendChild(Nav());
-document.querySelector(".category-container").append(createCategory());
 document.querySelector(".category-container").append(createCategory());
 document.querySelector(".filter-container").appendChild(createFilterBtn());
-document.getElementById("cartes").appendChild(displayProducts());
 
 let history = createBrowserHistory();
-let homeRequest = await fetch("http://192.168.5.181:3000/home");
+let homeRequest = await fetch('http://192.168.5.181:3000/home');
 let homeData = await homeRequest.json();
 
-if (history.location.pathname == "/") {
-  document.getElementById("cartes").appendChild(displayProducts(homeData));
-} else if (history.location.pathname == "/product/") {
-  const searchParams = new URLSearchParams(history.location.search);
-  const idDuProduit = searchParams.get("id");
+if (history.location.pathname == '/') {
+    document.getElementById("cartes").appendChild(displayProducts(homeData));
 
-  let productRequest = await fetch(
-    `http://192.168.5.181:3000/product/${idDuProduit}`
-  );
-  let productData = await productRequest.json();
 
-  document.getElementById("cartes").appendChild(displayProduct(productData[0]));
+} else if (history.location.pathname == '/product/') {
+    const searchParams = new URLSearchParams(history.location.search);
+    const idDuProduit = searchParams.get('id')
 
-  // document.getElementById('cartes').appendChild()
+    let productRequest = await fetch(`http://192.168.5.181:3000/product/${idDuProduit}`);
+    let productData = await productRequest.json();
+
+    document.getElementById("cartes").appendChild(displayProduct(productData[0]));
+
+} else if (history.location.pathname == '/panier/') { 
+
 }
